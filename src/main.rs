25bdc8c2bf7e39f69cb836dd4eddc2bd4158de24
Jan Starke <jan.starke@outofbed.org>
2021-12-17 10:50:53 +0100

    added more verbose warnings

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,40 @@ use clap::{App, Arg};
 use evtx::EvtxParser;
 use simple_logger::SimpleLogger;
 use std::path::PathBuf;
+
 use log;
+use std::io::Write;
 
 mod bodyfilevisitor;
 use bodyfilevisitor::BodyfileVisitor;
 
 fn main() {
+    let log_level = if cfg!(debug_assertions) {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Warn
+    };
+    env_logger::Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{}:{} {} [{}] - {}",
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Trace)
+        .init();
+
     SimpleLogger::new()
-        .with_level(log::LevelFilter::Warn)
+        .with_level(log_level)
+        .with_colors(true)
         .init()
         .unwrap();
+        
     let app = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -35,7 +59,9 @@ fn main() {
                 for record in parser.records_to_visitor(|| BodyfileVisitor::new()) {
                     match record {
                         Ok(r) => println!("{}", r),
-                        Err(e) => log::error!("{}", e),
+                        Err(e) => {
+                            log::error!("{}", e)
+                        },
                     }
                 }
             }
