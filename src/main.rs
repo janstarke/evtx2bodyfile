4f5bb4010f804ba829b744ffe07a6e7ccda33d9f
Jan Starke <jan.starke@outofbed.org>
2022-02-28 19:22:09 +0100

    complete rewrite, to use json as output format

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use clap::{App, Arg};
-use evtx::EvtxParser;
-use std::path::PathBuf;
+use evtx::{EvtxParser, SerializedEvtxRecord};
+use serde::Serialize;
+use serde_json::{Value, json};
+use std::{path::PathBuf, collections::HashMap};
+use bodyfile::Bodyfile3Line;
+use anyhow::{Result, anyhow};
 
 use log;
 use std::io::Write;
 
-mod bodyfilevisitor;
-use bodyfilevisitor::BodyfileVisitor;
-
 fn main() {
     let log_level = if cfg!(debug_assertions) {
-        log::LevelFilter::Debug
+        log::LevelFilter::Warn
     } else {
         log::LevelFilter::Warn
     };
@@ -49,12 +50,13 @@ fn main() {
         let fp = PathBuf::from(file);
         match EvtxParser::from_path(fp) {
             Ok(mut parser) => {
-                for record in parser.records_to_visitor(|| BodyfileVisitor::new()) {
-                    match record {
-                        Ok(r) => println!("{}", r),
-                        Err(e) => {
-                            log::error!("{}", e)
-                        },
+                for record_r in parser.records_json_value() {
+                    match record_r {
+                        Err(why) => log::warn!("{}", why),
+                        Ok(value) => match record_to_mactime(value) {
+                            Err(why) => log::warn!("{}", why),
+                            Ok(line) => println!("{}", line),
+                        }
                     }
                 }
             }
@@ -63,4 +65,75 @@ fn main() {
             }
         }
     }
+}
+#[derive(Serialize)]
+struct BfData<'a> {
+    event_record_id: u64,
+    event_id: &'a Value,
+    provider_name: &'a Value,
+	channel_name: &'a Value,
+    activity_id: Option<&'a Value>,
+    custom_data: HashMap<&'a String, &'a Value>
+}
+
+macro_rules! from_json {
+    ($value: ident, $( $att:expr ),+ ) => {
+        {
+            let mut value = $value;
+            $(
+                value = value.get($att).ok_or(anyhow!("missing '{}' key in {}", $att, value))?;
+            )+
+            value
+        }
+    };
+}
+
+impl<'a> BfData<'a> {
+    pub fn from(event_record_id: u64, value: &'a Value) -> Result<Self> {
+        let event = from_json!(value, "Event");
+        let system = from_json!(event, "System");
+        let event_id = {
+            let event_id = from_json!(system, "EventID");
+            match event_id.get("#text") {
+                Some(eid) => eid,
+                None => event_id
+            }
+        };
+        
+
+        let provider_name= from_json!(system, "Provider", "#attributes", "Name");
+        let channel_name = from_json!(system, "Channel");
+
+        let activity_id = system.get("Correlation")
+                                                .and_then(|c|c.get("#attributes"))
+                                                .and_then(|c|c.get("ActivityId"));
+        
+        let mut custom_data = HashMap::new();
+        match event {
+            Value::Object(contents) => {
+                for (key, value) in contents.iter() {
+                    if key != "System" && key != "#attributes" {
+                        custom_data.insert(key, value);
+                    }
+                }
+            }
+            _ => ()
+        }
+        Ok(Self {
+            event_record_id,
+            event_id,
+            provider_name,
+            channel_name,
+            activity_id,
+            custom_data
+        })
+    }
+}
+
+fn record_to_mactime(record: SerializedEvtxRecord<Value>) -> Result<String> {
+    let bf_data = BfData::from(record.event_record_id, &record.data)?;
+    let bf_line = Bodyfile3Line::new()
+        .with_mtime(record.timestamp.timestamp())
+        .with_owned_name(json!(bf_data).to_string());
+    Ok(bf_line.to_string())
 }
